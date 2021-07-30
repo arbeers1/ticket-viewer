def8b9883ff4568186846d2675ad9ce08303b928
ALEXANDER BEERS <arbeers@wisc.edu>
2021-07-29 20:16:33 -0500

    Added parse capability for ticket information to be displayed in a list

--- src/ticket_viewer.py
+++ src/ticket_viewer.py
@@ -2,12 +2,13 @@
 #Description: Retrieve zendesk ticket details attached to an account
 #Author: Alex Beers
 
+from datetime import timezone
 import requests
+import datetime
 import config
 
 class TicketViewer():
     
-    #TODO: Get information from all possible ticket pages and return it in a list of pages, make page size 25
     def get_tickets():
         """
         Retrieves a list of tickets associated with the zendesk account.
@@ -17,12 +18,13 @@ class TicketViewer():
         
         Returns
             If operation is successful:
-            (True, json result)
+            (True, [json page 1, json page 2, ...]) where each json page has a max of 100 tickets
             If operation fails:
             (False, displayable error string)
         """
         url_to_get = 'https://zcctreehugger.zendesk.com/api/v2/tickets.json'
         final_result = []
+        #Loop through each page
         while(True):
             try:
                 response = requests.get(
@@ -48,3 +50,99 @@ class TicketViewer():
                 return (False, "An error has been encountered:\n" + str(response.status_code) + "\n" + response.reason)
         return(True, final_result)
 
+    def parse_tickets_simple(ticket_json, start_index, end_index=None):
+        """
+        Parses json ticket list and provides simple information about an individual ticket or a range of tickets.
+        This method is intended to only return data useful for displaying tickets in a list. For more in depth 
+        information on individual tickets, use parse_tickets_detailed().
+
+        Paramaters
+            ticket_json - A single page json as returned by get_tickets()
+            start_index - The index ticket to start parsing
+            end_index - (Optional) If provided method will parse between start/end index
+                                   Otherwise method will only parse start_index
+
+        Returns
+            [(ticket_details_1), (ticket_details2), ...]
+            where each ticket_details_n is a tuple formatted (priority, status, id, subject, requester, requester_updated, assignee)
+
+        Throws
+            ValueError if start/end index is not in range 0-99
+            IndexError if provided index is out of bounds of the provided json 'tickets' array
+            LookupError if user name look up fails
+        """
+        if(end_index == None):
+            end_index = start_index + 1
+        if(start_index < 0 or start_index > 99 or end_index < 0 or end_index > 99):
+            raise ValueError("Index is out of page boundries")
+
+        tickets = ticket_json['tickets']
+        final_list = []
+        for x in range(start_index, end_index):
+            priority = tickets[x]['priority']
+            status = tickets[x]['status']
+            id = tickets[x]['id']
+            subject = tickets[x]['subject']
+            requester_name = TicketViewer._user_name(tickets[x]['requester_id'])
+            if(requester_name[0]):
+                requester_name = requester_name[1]
+            else:
+                raise LookupError(requester_name[1])
+            time = TicketViewer._time_delta(tickets[x]['updated_at'])
+            assignee_name = TicketViewer._user_name(tickets[x]['assignee_id'])
+            if(assignee_name[0]):
+                assignee_name = assignee_name[1]
+            else:
+                raise LookupError(assignee_name[1])
+            final_list.append((priority, status, id, subject, requester_name, time, assignee_name))
+        return final_list
+            
+                 
+
+    def _user_name(user_id):
+        """
+        Helper method for parsing tickets to retrieve the displayed user's name from an id.
+
+        Paramaters
+            user_id - an integer id number associated with a user
+
+        Returns
+            If operation is successful:
+            (True, user_name)
+            If operation fails:
+            (False, displayable error string)
+
+        """
+        try:
+            response = requests.get(
+                url= 'https://zcctreehugger.zendesk.com/api/v2/users/' + str(user_id),
+                auth=('arbeers@wisc.edu', config.passw),
+                timeout=60)
+        except ConnectionError:
+            return (False, "An error has been encountered with the network :(")
+        except TimeoutError:
+            return (False, "Failed to retrieve a response from the server :(")
+
+        #Return username if api call successful
+        if(response.status_code >= 200 and response.status_code < 300):
+            return (True, response.json()['user']['name'])
+        else:
+            return (False, "An error has been encountered:\n" + str(response.status_code) + "\n" + response.reason)
+
+    def _time_delta(time):
+        """
+        Helper method to find the time difference since ticket last updated.
+
+        Paramaters
+            time - the utc time string as returned by the get_tickets() json
+
+        Returns
+            string with time difference
+        """
+        orig_time = datetime.datetime(int(time[0:4]), int(time[5:7]), int(time[8:10]), int(time[11:13]),
+                                      int(time[14:16]), int(time[17:19]), tzinfo=timezone.utc)
+        delta = datetime.datetime.now(timezone.utc) - orig_time
+        if(delta.days > 0):
+            return str(delta.days) + ' day(s) ago'
+        else:
+            return str(int(delta.seconds/3600)) + ' hour(s) ago'
--- tests/ticket_tests.py
+++ tests/ticket_tests.py
@@ -3,7 +3,9 @@
 #Author: Alex Beers
 
 import unittest
+from datetime import datetime
 import sys
+from unittest.suite import TestSuite
 sys.path[0] = sys.path[0].replace('tests', 'src')
 from ticket_viewer import TicketViewer as tv
 
@@ -19,6 +21,73 @@ class TestTicketRetrieval(unittest.TestCase):
         self.assertEqual(len(result[1]), 2)
         self.assertEqual(len(result[1][0]['tickets']), 100)
         self.assertEqual(len(result[1][1]['tickets']), 1)
+
+    def test_time_delta(self):
+        """
+        Test the helper method _time_delta() to ensure an accurate delta is returned
+        """
+        result = tv._time_delta("2021-07-20T20:29:47Z")
+        self.assertTrue('day' in result)
+        self.assertTrue(int(result[0:1]) > 5)
+
+        time = datetime.utcnow()
+        self.assertEqual('0 hour(s) ago', tv._time_delta(str(time)))
+
+    def test_user_lookup(self):
+        """
+        Test the help method _user_name() 
+        """
+        result = tv._user_name(1265123600889)
+        self.assertTrue(result[0])
+        self.assertEqual(result[1], 'Alex Beers')
+
+        result = tv._user_name(1265063393490)
+        self.assertTrue(result[0])
+        self.assertEqual(result[1], 'The Customer')
+
+        result = tv._user_name(-1)
+        self.assertFalse(result[0])
+
+    def test_simple_parse_individual(self):
+        """
+        Test the simple ticket parse (one ticket)
+        """
+        result = tv.get_tickets()
+        assert(result[0] == True)
+        parse = tv.parse_tickets_simple(result[1][0], 0)
+        self.assertEqual(len(parse), 1)
+        self.assertEqual(parse[0][0], 'normal')
+        self.assertEqual(parse[0][1], 'open')
+        self.assertEqual(parse[0][2], 1)
+        self.assertEqual(parse[0][3], 'Sample ticket: Meet the ticket')
+        self.assertEqual(parse[0][4], 'The Customer')
+        self.assertTrue('day' in parse[0][5])
+        self.assertEqual(parse[0][6], 'Alex Beers')
+
+    def test_simple_parse_multiple(self):
+        """
+        Test the simple ticket parse (multiple tickets)
+        """
+        result = tv.get_tickets()
+        assert(result[0] == True)
+        parse = tv.parse_tickets_simple(result[1][0], 0, end_index=5)
+        self.assertEqual(len(parse), 5)
+        for x in parse:
+            self.assertTrue(parse[0][2] < 7)
+
+    def test_simple_parse_errors_raised(self):
+        """
+        Test that the appropriate errors are raised in simple parse
+        """
+        result = tv.get_tickets()
+        with(self.assertRaises(ValueError)):
+            tv.parse_tickets_simple(result[1][0], 90, end_index=110)
+        with(self.assertRaises(ValueError)):
+            tv.parse_tickets_simple(result[1][0], 110)
+
+        with(self.assertRaises(IndexError)):
+            tv.parse_tickets_simple(result[1][1], 0, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
