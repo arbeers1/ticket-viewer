f128defa8dd1843a8326eda1adceb62ef9e79508
ALEXANDER BEERS <arbeers@wisc.edu>
2021-07-31 14:45:55 -0500

    Ticket pages are now cached in browser, and periodically updated, to allow for faster loads

--- static/ticket_displayer.js
+++ static/ticket_displayer.js
@@ -2,30 +2,40 @@
 //Description: Link backend to frontend, Extracts ticket data
 //Author: Alex Beers
 
+var total_pages = 0;
+var curr_page = -1;
+var page_cache = [] //This is a page cache list which will periodically update.
+                    //Helps reduce load times by periodically updating the cache vs. calling api on user demand
+
+window.onload = cache_tickets(); //Starts cache system
+
 /**
  * Calls the flask app to get the data for the desired page.
  * @param {*} page_num - page number to display
+ * @param cahce - True or False, if True then data is stored in page_cache, otherwise display_tickets is auto called
  */
-var total_pages = 0;
-var curr_page = -1;
-
-function get_tickets(page_num){
-    if(page_num == 1){
-        $("#prev").attr("disabled","disabled");
-    }else{
-        $("#prev").removeAttr("disabled");
-    }
-    if(page_num == total_pages){
-        $("#next").attr("disabled","disabled");
-    }else{
-        $("#next").removeAttr("disabled");
-    }
+function get_tickets(page_num, cache){
     $.getJSON($SCRIPT_ROOT + '/get_tickets', {
         page: page_num
     }, function(data) {
         curr_page = page_num;
-        display_tickets(data);
+        if(!cache){
+            display_tickets(data);
+        }else{
+            page_cache[page_num - 1] = data;
+        }
     }); 
+}
+
+/**
+ * Caches tickets every 10 seconds into page_cache
+ */
+function cache_tickets(){
+    setInterval(function(){
+        for(var i = 0; i < total_pages; i++){
+            get_tickets(i + 1, true);
+        }
+    }, 10000)
 }
 
 /**
@@ -33,8 +43,18 @@ function get_tickets(page_num){
  * @param {*} tks - The json returned from Flask's get_tickets call
  */
 function display_tickets(tks){
-    $('#count').text('Total Tickets: ' + tks.count);
-    total_pages = Math.ceil(tks.count / 25); 
+    total_pages = Math.ceil(tks.count / 25);
+    if(curr_page == 1){
+        $("#prev").attr("disabled","disabled");
+    }else{
+        $("#prev").removeAttr("disabled");
+    }
+    if(curr_page == total_pages){
+        $("#next").attr("disabled","disabled");
+    }else{
+        $("#next").removeAttr("disabled");
+    }
+    $('#count').text('Total Tickets: ' + tks.count); 
     $('#page_count').text('Displaying page ' + curr_page + ' of ' + total_pages);
     $('#tv tbody').empty();
     for(var i = 0; i < tks.tickets.length; i++){
@@ -45,7 +65,7 @@ function display_tickets(tks){
         '<td>' + tks.tickets[i].subject + '</td>' +
         '<td>' + tks.tickets[i].requester_name + '</td>' +
         '<td>' + tks.tickets[i].requester_updated + '</td>' +
-        '<td>' + tks.tickets[i].assignee + '</td>' +
+        '<td>' + tks.tickets[i].assignee_name + '</td>' +
         '</tr>');
     }
 }
@@ -54,26 +74,50 @@ function display_tickets(tks){
  * Displays the next page of results in the table
  */
 function next(){
-    get_tickets(curr_page + 1);
+    if(curr_page < total_pages){
+        curr_page++;
+    }
+    if(page_cache[curr_page - 1] !== undefined){
+        display_tickets(page_cache[curr_page - 1]);
+    }else{
+        get_tickets(curr_page, false);
+    }
 }
 
 /**
  * Displays the previous page of results in the table
  */
 function prev(){
-    get_tickets(curr_page - 1);
+    if(curr_page > 1){
+        curr_page--;
+    }
+    if(page_cache[curr_page - 1] !== undefined){
+        display_tickets(page_cache[curr_page - 1]);
+    }else{
+        get_tickets(curr_page, false);
+    }
 }
 
 /**
  * Displays the first page of results in the table
  */
 function first(){
-   get_tickets(1);
+    curr_page = 1;
+    if(page_cache[curr_page - 1] !== undefined){
+        display_tickets(page_cache[curr_page - 1]);
+    }else{
+        get_tickets(curr_page, false);
+    }
 }
 
 /**
  * Displays the last page of results in the table
  */
 function last(){
-    get_tickets(total_pages);
+    curr_page = total_pages;
+    if(page_cache[curr_page - 1] !== undefined){
+        display_tickets(page_cache[curr_page - 1]);
+    }else{
+        get_tickets(curr_page, false);
+    }
 }
