a9646befbc4226ee0917e76485a2a4be05a0c94e
ALEXANDER BEERS <arbeers@wisc.edu>
2021-07-31 11:34:09 -0500

    app now maintains a user cahce when getting tickets

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify, request
 from src.ticket_viewer import TicketViewer as tv
-from src import config
 
 #Start application
 app = Flask(__name__)
@@ -11,14 +10,14 @@ def index():
 
 @app.route('/get_tickets', methods=['GET'])
 def tickets():
-    print('here')
     tks = tv.get_tickets(request.args.get('page'))
     if(not tks[0]):
         #TODO: Return error page
         raise NotImplementedError
     
     result = []
+    user_cache = []
     for x in range (len(tks[1]['tickets'])):
-        result.append(tv.parse_ticket_simple(tks[1], x))
-    print('return tickets')
+        result.append(tv.parse_ticket_simple(tks[1], x, user_cache))
     return jsonify(tickets = result)
+
